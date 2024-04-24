ec7aca91443fc13bdd281a0e7d25a4f529e80d55
ahadnur <ahadnur44@gmail.com>
2024-04-24 23:38:46 +0600

    G-tag updated on documents

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,9 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
