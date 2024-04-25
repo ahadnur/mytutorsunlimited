26fc62ef43a03dc885e328f3675e55d7f0103c7a
ahadnur <ahadnur44@gmail.com>
2024-04-25 23:15:11 +0600

    Google Tags added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,8 +16,9 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <Script>
-        {`
+      <Head>
+        <Script>
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -26,12 +27,30 @@ export default function App({ Component, pageProps }) {
             page_path: window.location.pathname,
           });    
           `}
-      </Script>
-    <Head>
-      <title>My Tutors Unlimited</title>
-    </Head>
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script>
+          {`
+
+          
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KNB5WW7J');
+          
+
+        `}
+        </Script>
+        {/* End Google Tag Manager */}
+
+        <title>My Tutors Unlimited</title>
+      </Head>
       <Layout className={poppins.className}>
-        <main className={`${poppins.className} relative mt-[73px] container mx-auto py-16`}>
+        <main
+          className={`${poppins.className} relative mt-[73px] container mx-auto py-16`}
+        >
           <Component {...pageProps} />
         </main>
       </Layout>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNB5WW7J"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
