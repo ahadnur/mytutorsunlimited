91afb24de6461b26496866357892a32bced9f527
ahadnur <ahadnur44@gmail.com>
2024-05-02 00:57:19 +0600

    updated appjs

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,9 +15,6 @@ function Hero() {
         <h1 style={{lineHeight: "1"}} className="text-6xl 2xl:text-8xl text-center md:text-left tracking-tighter leading-none mb-6 text-slate-900 font-black">
         Personalized Online <span className="text-blue-500">Tutoring</span> Services
         </h1>
-        {/* <p className="mb-5 text-slate-700 font-extralight w-full lg:w-3/4 lg:text-lg xl:text-xl text-center md:text-left">
-        Embark on coding adventures with us! Fun,interactive lessons empower kids to create, learn, and master programming effortlessly in <strong>PYTHON, C, C++, JAVASCRIPT</strong>.
-        </p> */}
         <Link href={`#course`} aria-label="Find a Tutor"><Button className="px-10 py-6 text-md lg:text-lg mt-6">Find a Tutor</Button></Link>
       </div>
       <div className="w-full flex justify-center md:justify-end">
