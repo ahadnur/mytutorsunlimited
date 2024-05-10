620220a00a4d1abd6fee5d66d0266cedc7f0c70c
ahadnur <ahadnur44@gmail.com>
2024-05-11 00:50:10 +0600

    fixed bugs on analytics

--- src/components/FacebookMsg.js
+++ src/components/FacebookMsg.js
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 function FacebookMsg() {
   return (
     <FacebookProvider appId={process.env.NEXT_PUBLIC_FACEBOOK_APP_ID} chatSupport>
-        <CustomChat  pageId={process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID} minimized={false} />
+        <CustomChat  pageId={process.env.NEXT_PUBLIC_FACEBOOK_PAGE_ID} />
       </FacebookProvider>
   )
 }
